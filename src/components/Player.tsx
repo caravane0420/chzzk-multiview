4abb61d74b28cd755df3980d804b780c8d20f4a3
caravane0420 <caravane0420@gmail.com>
2026-03-31 20:20:17 +0900

    fix: 플레이어 iframe 주소 수정 및 404 에러 해결

--- src/components/Player.tsx
+++ src/components/Player.tsx
@@ -9,7 +9,7 @@ const Player: React.FC<PlayerProps> = ({ channelId }) => {
     <div className="relative w-full h-full bg-[#0A0514] rounded-lg overflow-hidden border border-white/10 shadow-[0_4px_30px_rgba(139,92,246,0.15)] transition-all duration-300">
       {/* 치지직 공식 iframe 로드 */}
       <iframe
-        src={`https://chzzk.naver.com/live/${channelId}/iframe?autoplay=true&muted=true&target=browser`}
+        src={`https://chzzk.naver.com/live/${channelId}`}
         title={`Stelview Player - ${channelId}`}
         allow="autoplay; fullscreen"
         allowFullScreen
